5e9af07aa1dc259030be67f2d9e97e3e720d8a87
Fluffy0507 <martyna.techniek@gmail.com>
2025-02-17 13:21:19 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    voeding += 5
+})
+input.onButtonPressed(Button.B, function () {
+    knuffel += 5
+})
+let voeding = 20
+let knuffel = 20
+let dood = 0
+basic.forever(function () {
+    basic.pause(1000)
+    voeding += -1
+    knuffel += -1
+    dood += -1
+})
+basic.forever(function () {
+    if (true) {
+    	
+    } else {
+    	
+    }
+})
